5238feda9fcd384f04503d5227b7d341c0c053f6
Rijin Mk <rijin.mk9@gmail.com>
2025-04-06 23:28:47 +0400

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Coming Soon</h1>
+        <h2>Aswati</h2>
+      </main>
+    </div>
+  );
+}
